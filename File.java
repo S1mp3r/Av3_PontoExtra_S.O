aebbfdd446843f59dd273fe5c080944fb40b4157
S1mp3r <99444306+S1mp3r@users.noreply.github.com>
2024-06-11 21:36:19 -0300

    Update File.java

--- File.java
+++ File.java
@@ -1,10 +1,12 @@
 public class File {
     private String name;
     private String content;
+    private Integer qtt;
 
     public File(String name) {
         this.name = name;
         this.content = "";
+        this.qtt = -1;
     }
 
     public String getName() {
@@ -21,5 +23,17 @@ public class File {
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public Integer getQtt() {
+        return qtt;
+    }
+
+    public void setQtt(Integer qtt) {
+        this.qtt = qtt;
+    }
+
+    public void incrementalQtt() {
+        this.qtt++;
     }
 }
